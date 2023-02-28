e26879073e4a670ad99b07af9cd106317314ca8f
Diego178 <zS20018149@estudiantes.uv.mx>
2023-02-28 11:08:20 -0600

    tarea

--- practica02/Services/Operaciones.cs
+++ practica02/Services/Operaciones.cs
@@ -3,12 +3,32 @@ using WSDL.mensajes;
 
 namespace WSDL.operaciones{
     public class Operaciones : Mensajes{
+        private string[] mensajes = new string[2];
+ 
         public string Saludar(string nombre){
-            string msj = "Hola "+nombre;
-            return msj;
+            Boolean estaLleno = true;
+            for(int x = 0; x < mensajes.Length; x++){
+                if(string.IsNullOrEmpty(mensajes[x])){
+                    mensajes[x]=nombre;
+                    estaLleno = false;
+                    break;
+                }
+            }
+
+            if (estaLleno == true){
+                return "Memoria llena..";
+            }
+            return "Hola "+nombre;
         }
         public string Mostrar(int id){
-            return "x";
+            if(id >= mensajes.Length || id < 0){
+                return "No existe ese id en los limites del array";
+            } 
+            
+            if(!string.IsNullOrEmpty(mensajes[id])){
+                return "hola " + mensajes[id] + " que weno q volviste";
+            }
+            return "No se encontro un mensaje con este id";
         }
     }
 }
